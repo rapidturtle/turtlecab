9bd06ad922785791b02daaba1e78b93c8cd93d5c
Andrew Porter <aporter@rapidturtle.com>
2025-07-14 16:35:26 -0500

    Start work on the home page

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { all } from '@awesome.me/kit-92e90780a1/icons'
+
+library.add(all)
+
 const app = createApp(App)
 
 app.use(router)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
