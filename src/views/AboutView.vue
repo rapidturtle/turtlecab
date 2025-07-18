5001b20e3b03f6f65d06dec989f786a58a80175b
Andrew Porter <aporter@rapidturtle.com>
2025-07-18 12:59:01 -0500

    Update the About page
    Changed the layout and added Jinx.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,91 +1,52 @@
 <script setup lang="ts">
+import BaseTextGradient from '@/components/base/BaseTextGradient.vue'
 import imageAndy from '@/assets/images/andy.jpg'
+import imageJinx from '@/assets/images/jinx.jpg'
 
-const benefits = [
-  'I built this site myself',
-  'Big fan of Indian, Thai, and Cajun',
-  'Music, silence, or audiobook—your choice',
-  'Jinx the cockapoo rides on request',
-  'Favorite show? <em>Avatar: The Last Airbender</em>',
-  'I probably won’t wear the suit from the photo 😉',
+const people = [
+  {
+    name: 'Andrew Porter',
+    role: 'Driver & Founder',
+    imageUrl: imageAndy,
+  },
+  {
+    name: 'Jinx',
+    role: 'Co-Pilot',
+    imageUrl: imageJinx,
+  },
 ]
 </script>
 
 <template>
-  <div class="overflow-hidden bg-stone-900 py-24 sm:py-32">
-    <div class="relative isolate">
-      <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div
-          class="mx-auto flex max-w-2xl flex-col gap-16 bg-white/[0.03] px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20"
-        >
+  <div class="bg-stone-900 py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 text-center lg:px-8">
+      <div class="mx-auto max-w-2xl">
+        <h2 class="text-5xl font-semibold tracking-tight text-balance text-white sm:text-6xl">
+          Meet your driver <BaseTextGradient class="no-wrap">(and co-pilot)</BaseTextGradient>
+        </h2>
+        <p class="mt-6 text-lg text-stone-400 sm:text-xl/8">
+          I’m Andy—founder and driver of Turtle Cab. I started this service to offer a friendly,
+          reliable alternative to rideshare apps. Whether you’re headed to the airport, across town,
+          or out for the day, I’ll make sure you get there safely and comfortably. And if you’re
+          lucky, Jinx might join the ride too.
+        </p>
+      </div>
+      <ul
+        role="list"
+        class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"
+      >
+        <li v-for="person in people" :key="person.name" class="rounded-2xl bg-stone-800 px-8 py-10">
           <img
-            class="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-            :src="imageAndy"
+            class="mx-auto size-48 rounded-full outline-1 -outline-offset-1 outline-white/10 md:size-56"
+            :src="person.imageUrl"
             alt=""
           />
-          <div class="w-full flex-auto">
-            <h2 class="text-5xl font-semibold tracking-tight text-white sm:text-7xl">
-              Meet your driver
-            </h2>
-            <p class="mt-8 text-lg font-medium text-pretty text-stone-400 sm:text-xl/8">
-              Hey, I’m Andy—founder and driver behind Turtle Cab. I live in Hendersonville and
-              started this service to offer a more personal, trustworthy alternative to rideshare
-              apps. Whether you’re headed to the airport or just across town, I’m here to make your
-              ride comfortable, reliable, and stress-free.
-            </p>
-            <ul
-              role="list"
-              class="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base/7 text-stone-200 sm:grid-cols-2"
-            >
-              <li v-for="benefit in benefits" :key="benefit" class="flex gap-x-3">
-                <font-awesome-icon
-                  :icon="['fadr', 'circle-check']"
-                  class="mt-1 text-white"
-                  fixed-width
-                />
-                <span v-html="benefit"></span>
-              </li>
-            </ul>
-            <div class="mt-10 flex">
-              <RouterLink
-                :to="{ name: 'rides' }"
-                class="text-primary-400 hover:text-primary-300 flex items-center gap-x-2 text-sm/6 font-semibold"
-              >
-                <span>Book a ride</span>
-                <font-awesome-icon :icon="['fadr', 'arrow-right']" aria-hidden="true" />
-              </RouterLink>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div
-        class="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
-        aria-hidden="true"
-      >
-        <div
-          class="aspect-1318/752 w-329.5 flex-none bg-linear-to-r from-[var(--color-lightning-yellow-500)] to-[var(--color-blue-ribbon-500)] opacity-20"
-          style="
-            clip-path: polygon(
-              73.6% 51.7%,
-              91.7% 11.8%,
-              100% 46.4%,
-              97.4% 82.2%,
-              92.5% 84.9%,
-              75.7% 64%,
-              55.3% 47.5%,
-              46.5% 49.4%,
-              45% 62.9%,
-              50.3% 87.2%,
-              21.3% 64.1%,
-              0.1% 100%,
-              5.4% 51.1%,
-              21.4% 63.9%,
-              58.9% 0.2%,
-              73.6% 51.7%
-            );
-          "
-        />
-      </div>
+          <h3 class="mt-6 text-base/7 font-semibold tracking-tight text-white">
+            {{ person.name }}
+          </h3>
+          <p class="text-sm/6 text-stone-400">{{ person.role }}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
