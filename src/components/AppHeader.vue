8b20c86c8eed3aa080f530d4c3337e13e207db13
Andrew Porter <aporter@rapidturtle.com>
2025-07-18 11:11:00 -0500

    Add transition to the mobile menu
    … and close the menu when a link is clicked.

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Dialog, DialogPanel } from '@headlessui/vue'
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel } from '@headlessui/vue'
 import IconTurtleCab from '@/components/icons/IconTurtleCab.vue'
 
 const navigation = [
@@ -46,50 +46,86 @@ const mobileMenuOpen = ref(false)
           :to="{ name: 'rides' }"
           class="bg-primary-500 hover:bg-primary-400 focus-visible:outline-primary-500 inline-flex items-center gap-x-2 rounded-full px-2.5 py-1 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
           >Book a ride
-          <font-awesome-icon :icon="['fadr', 'arrow-right']" class="-mr-0.5" aria-hidden="true"
+          <font-awesome-icon :icon="['fadr', 'car-side']" class="-mr-0.5" aria-hidden="true"
         /></RouterLink>
       </div>
     </nav>
-    <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-      <div class="fixed inset-0 z-50" />
-      <DialogPanel
-        class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-stone-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
-      >
-        <div class="flex items-center justify-between">
-          <RouterLink :to="{ name: 'home' }" class="-m-1.5 p-1.5">
-            <span class="sr-only">Turtle Cab</span>
-            <IconTurtleCab class="h-8 w-auto" dark-mode />
-          </RouterLink>
-          <button
-            type="button"
-            class="-m-2.5 rounded-md p-2.5 text-stone-400"
-            @click="mobileMenuOpen = false"
+    <TransitionRoot :show="mobileMenuOpen" as="template">
+      <Dialog class="lg:hidden" @close="mobileMenuOpen = false">
+        <TransitionChild
+          as="template"
+          enter="transition-opacity ease-linear duration-300"
+          enter-from="opacity-0"
+          enter-to="opacity-100"
+          leave="transition-opacity ease-linear duration-300"
+          leave-from="opacity-100"
+          leave-to="opacity-0"
+        >
+          <div class="fixed inset-0 z-40 bg-black/50" />
+        </TransitionChild>
+
+        <div class="fixed inset-0 z-50" />
+        <TransitionChild
+          as="template"
+          enter="transition ease-in-out duration-300 transform"
+          enter-from="translate-x-full"
+          enter-to="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leave-from="translate-x-0"
+          leave-to="translate-x-full"
+        >
+          <DialogPanel
+            class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-stone-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-white/10"
           >
-            <span class="sr-only">Close menu</span>
-            <font-awesome-icon :icon="['fadr', 'xmark']" />
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-stone-500/25">
-            <div class="space-y-2 py-6">
+            <div class="flex items-center justify-between">
               <RouterLink
-                v-for="item in navigation"
-                :key="item.name"
-                :to="item.href"
-                class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-stone-800"
-                >{{ item.name }}</RouterLink
+                :to="{ name: 'home' }"
+                class="-m-1.5 p-1.5"
+                @click="mobileMenuOpen = false"
               >
-            </div>
-            <div class="py-6">
-              <RouterLink
-                :to="{ name: 'rides' }"
-                class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-stone-800"
-                >Book a ride</RouterLink
+                <span class="sr-only">Turtle Cab</span>
+                <IconTurtleCab class="h-8 w-auto" dark-mode />
+              </RouterLink>
+              <button
+                type="button"
+                class="-m-2.5 rounded-md p-2.5 text-stone-400"
+                @click="mobileMenuOpen = false"
               >
+                <span class="sr-only">Close menu</span>
+                <font-awesome-icon :icon="['fadr', 'xmark']" />
+              </button>
             </div>
-          </div>
-        </div>
-      </DialogPanel>
-    </Dialog>
+            <div class="mt-6 flow-root">
+              <div class="-my-6 divide-y divide-stone-500/25">
+                <div class="space-y-2 py-6">
+                  <RouterLink
+                    v-for="item in navigation"
+                    :key="item.name"
+                    :to="item.href"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-stone-800"
+                    active-class="bg-stone-800"
+                    @click="mobileMenuOpen = false"
+                    >{{ item.name }}</RouterLink
+                  >
+                </div>
+                <div class="py-6">
+                  <RouterLink
+                    :to="{ name: 'rides' }"
+                    class="bg-primary-500 -mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-white hover:bg-stone-800"
+                    @click="mobileMenuOpen = false"
+                    >Book a ride
+                    <font-awesome-icon
+                      :icon="['fadr', 'car-side']"
+                      class="ml-1"
+                      aria-hidden="true"
+                    />
+                  </RouterLink>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </TransitionChild>
+      </Dialog>
+    </TransitionRoot>
   </header>
 </template>
