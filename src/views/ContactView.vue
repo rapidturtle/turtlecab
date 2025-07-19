09b0f5714ab06682456c181061b5a3deca7b7ba1
Andrew Porter <aporter@rapidturtle.com>
2025-07-19 15:05:11 -0500

    Add some pizzaz to the Contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -36,7 +36,66 @@ const contactOptions = [
 </script>
 
 <template>
-  <section class="isolate bg-stone-900 px-6 py-24 sm:py-32 lg:px-8">
+  <section class="relative isolate overflow-hidden bg-stone-900 px-6 py-24 sm:py-32 lg:px-8">
+    <img
+      src="https://images.unsplash.com/photo-1693871619509-d439714233dc?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&blend=1c1917&sat=-100&exp=15&blend-mode=multiply"
+      alt="Young man calling on his mobile phone"
+      class="absolute inset-0 -z-10 size-full object-cover object-right md:object-center"
+    />
+    <div
+      class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+    >
+      <div
+        class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[var(--color-lightning-yellow-500)] to-[var(--color-blue-ribbon-500)] opacity-20"
+        style="
+          clip-path: polygon(
+            74.1% 44.1%,
+            100% 61.6%,
+            97.5% 26.9%,
+            85.5% 0.1%,
+            80.7% 2%,
+            72.5% 32.5%,
+            60.2% 62.4%,
+            52.4% 68.1%,
+            47.5% 58.3%,
+            45.2% 34.5%,
+            27.5% 76.7%,
+            0.1% 64.9%,
+            17.9% 100%,
+            27.6% 76.8%,
+            76.1% 97.7%,
+            74.1% 44.1%
+          );
+        "
+      />
+    </div>
+    <div
+      class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+    >
+      <div
+        class="aspect-1097/845 w-274.25 bg-linear-to-tr from-[var(--color-lightning-yellow-500)] to-[var(--color-blue-ribbon-500)] opacity-20"
+        style="
+          clip-path: polygon(
+            74.1% 44.1%,
+            100% 61.6%,
+            97.5% 26.9%,
+            85.5% 0.1%,
+            80.7% 2%,
+            72.5% 32.5%,
+            60.2% 62.4%,
+            52.4% 68.1%,
+            47.5% 58.3%,
+            45.2% 34.5%,
+            27.5% 76.7%,
+            0.1% 64.9%,
+            17.9% 100%,
+            27.6% 76.8%,
+            76.1% 97.7%,
+            74.1% 44.1%
+          );
+        "
+      />
+    </div>
     <div class="mx-auto max-w-2xl sm:text-center">
       <h2 class="text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">
         Get in touch
